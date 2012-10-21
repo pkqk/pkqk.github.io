3677509fb34a60128b352e1f17b63ab1f17e728c
Adam Sven Johnson <adam@pkqk.net>
2012-10-21 13:21:04 +0100

    cleaned up the indentation

--- fabfile.py
+++ fabfile.py
@@ -5,13 +5,15 @@ env.use_ssh_config = True
 @task(default=True)
 @hosts('pkqk')
 def update():
-  "Push to github and pull to hosting"
-  local("git push")
-  with cd('www/pkqk.net'):
-    run('git pull')
+    "Push to github and pull to hosting"
+    local("git push")
+    with cd('www/pkqk.net'):
+        run('git checkout style/composite.css')
+        run('git pull')
+    css()
+
 @task
 @hosts('pkqk')
 def css():
     with cd('www/pkqk.net/style'):
         run('cat basic.css layout.css mobile.css resume.css > composite.css')
-
